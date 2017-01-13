8ae66ca97c39ad3bc9c918a4865626aeecbd1a76
Xie Lieping <frankbp@gmail.com>
2017-01-13 20:18:14 +0800

    2.0版本，新增录屏功能和支持横屏

--- BugEase.podspec
+++ BugEase.podspec
@@ -10,7 +10,7 @@ Pod::Spec.new do |s|
   #
 
   s.name         = "BugEase"
-  s.version      = "1.2.4"
+  s.version      = "2.0.0"
   s.summary      = "ios端Bug反馈SDK."
 
   s.homepage     = "https://bugease.hz.netease.com"
--- BugEase_iOS.framework/Headers/BEManager.h
+++ BugEase_iOS.framework/Headers/BEManager.h
@@ -7,6 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 typedef NS_ENUM(NSInteger, BEModel){
     BEInvocationEventNone, // 0 直接启动
@@ -16,30 +17,69 @@ typedef NS_ENUM(NSInteger, BEModel){
     BEInvocationEventTwoFingerSwipe // 4 双指右滑启动
 };
 
+typedef NS_ENUM(NSUInteger,BEIssueType){
+
+    BEISSUE_BUG,      // 0 bug
+    BEISSUE_ADVICE,   // 1 建议
+    BEISSUE_CRASH,    // 2 crash
+};
+
 @interface BEManager : NSObject
+
+/**
+ * 集成SDK
+ * @param appKey SDK的appKey
+ * @param model  SDK启动方式
+ *
+ **/
 +(id)shareManagerWithAppKeys:(NSString *) appKey withModel:(NSUInteger) model;
 
-//用户账号信息接口
-+(void) setUserAccountInfo:(NSString*) userAccountInfo;
-
-//取消用户账号信息
-    
-+(void) clearUserAccountInfo;
-
-
-//设置应用日志路径
-
-+(void) setApplyLogPath:(NSString*) logPath;
-//设置应用日志大小
-+(void) setApplyLogSizeWithKB:(NSInteger) sizeWithKB;
-
-//设置自定义字段
-
+/**
+ * 设置自定义字段
+ * @param key   数据名
+ * @param value 数据
+ *
+ **/
 +(void) setUserData:(NSString*) key andValue:(NSString*) value;
 
-//自己直接调用API上传文件
+/**
+ * 用户账号信息接口
+ * @param userAccountInfo 用户账号信息
+ *
+ **/
++(void) setUserAccountInfo:(NSString*) userAccountInfo;
 
-+(void) sendFeedBackWithType:(NSUInteger)type andContent:(NSString*) feedContent andReporter:(NSString*)reporter andImagePath:(NSString*)imagePath andTags:(NSArray*) tags;
+/**
+ * 清除用户账号信息
+ *
+ **/
++(void) clearUserAccountInfo;
+
+/**
+ * 设置应用日志路径
+ * @param logPath 应用日志路径(必选)
+ *
+ **/
++(void) setAppLogPath:(NSString*) logPath;
+
+/**
+ * 设置Crash收集开关
+ * @param isOpen 是否收集Crash(必选)
+ * true收集，false不收集
+ *
+ **/
++(void) setTrackingCrash:(BOOL) isOpen;
+
+/**
+ * 直接调用API上传文件
+ * @param type 反馈类型(必选)
+ * @param feedContent 反馈内容(必选)
+ * @param reporter 反馈人(可选)
+ * @param imagePath 截图文件(可选)
+ * @param tags 标签列表(可选)
+ *
+ **/
++(void) sendFeedBack:(BEIssueType)type andContent:(NSString*) feedContent andReporter:(NSString*)reporter andImage:(UIImage*)image andTags:(NSArray*) tags;
 
 
 //Demo app 专用API
